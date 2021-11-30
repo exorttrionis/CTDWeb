23e760c96b6deb6f80b824e9704a00b0dc77bc2d
Thomas Vu <56103689+thomasvu1198@users.noreply.github.com>
2021-11-30 10:25:44 +0700

    Update admin.js

--- Home/static/script/admin.js
+++ Home/static/script/admin.js
@@ -415,3 +415,18 @@ $('#finish-task').on('click', function () {
 $(document).ready(function () {
     GetAllTaskList();
 });
+function Userlogout() {
+    $.ajax({
+        type: "GET",
+        url: "http://" + window.location.host + "/logout/",
+        success: function (response) {
+            if (response == "done") {
+                window.location.href = "http://" + window.location.host;
+            }
+        }
+    })
+};
+
+$('#logout').on('click', function () {
+    Userlogout();
+})
